84b2129a14fd553fa1be478000335691685a94e7
Amanda <main@Amandas-MacBook-Air.local>
2020-08-11 03:57:18 -0400

    builds purposes_controller.rb

--- backend/app/controllers/purposes_controller.rb
+++ backend/app/controllers/purposes_controller.rb
@@ -1,2 +1,46 @@
 class PurposesController < ApplicationController
+  before_action :set_product
+
+  def index
+    purposes = @product.purposes 
+    render json: purposes
+  end
+  
+  def show 
+    purpose = @product.purposes.find_by(id: params[:id])
+    #purpose = Purpose.find(params[:id])
+    render json: purpose
+  end
+
+  def create 
+    purpose = @product.purposes.build(purpose_params)
+
+    if purpose.save 
+      render json: purpose   
+    else   
+      render json: purpose.errors, status: unprocessable_entity # {error: 'Error creating'}
+    end
+  
+  end
+  
+  # def update 
+    
+  # end
+
+  def destroy
+    purpose = Purpose.find(params[:id])
+    purpose.delete
+    render json: {purposeId: purpose.id}
+  end
+  
+  private 
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
+
+  def purpose_params 
+    params.require(:purpose).permit(:body, :product_id)
+  end
+
 end
