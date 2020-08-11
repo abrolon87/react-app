1a8e421bac507f6721053ea98a9647abadbb0e2c
Amanda <main@Amandas-MacBook-Air.local>
2020-08-11 02:49:12 -0400

    adds actions and strong params to products_controller.rb

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -10,4 +10,31 @@ class ProductsController < ApplicationController
     render json: product
   end
 
+  def create 
+    product = Product.new(product_params)
+
+    if product.save 
+      render json: product   
+    else   
+      render json: product.errors, status: unprocessable_entity # {error: 'Error creating'}
+    end
+  
+  end
+  
+  # def update 
+    
+  # end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.delete
+    render json: {productId: product.id}
+  end
+  
+  private 
+
+  def product_params 
+    params.require(:product).permit(:name)
+  end
+
 end
