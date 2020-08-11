90d94929a18fe1be2264e6b9f6b6f4ab269e5ca3
Amanda <main@Amandas-MacBook-Air.local>
2020-08-11 04:41:55 -0400

    test fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3001/products', {
+      method: 'GET'
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Home Remedies
+      </div>
+    )
+  }
 }
 
 export default App;
