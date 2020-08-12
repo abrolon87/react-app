7847d4802d080344924ab52c3dd2e5535a04fd05
Amanda <main@Amandas-MacBook-Air.local>
2020-08-12 16:54:13 -0400

    updates productReducer with ADDING_PRODUCTS and PRODUCT_ADDED

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -12,6 +12,20 @@ export default (state = {products: [], loading: false}, action) => {
         products: action.payload,
         loading: false
       }
+
+    case "ADDING_PRODUCTS":
+      return {
+        ...state,
+        loading: true
+      }
+
+    case "PRODUCT_ADDED":
+      return {
+        ...state,
+        products: [...state.products, action.payload],
+        loading: false
+      }
+
     default: 
       return state
   }
