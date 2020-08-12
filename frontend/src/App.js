5ee70e8c5af2e71bb13bcbb28511338c5ee325d4
Amanda <main@Amandas-MacBook-Air.local>
2020-08-12 04:02:51 -0400

    adds componentDidMount()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
+import {getProducts} from './actions/products'
 // import logo from './logo.svg';
 // import './App.css';
 
 class App extends Component {
 
-  // componentDidMount() {
-  //   fetch('http://localhost:3001/products', {
-  //     method: 'GET'
-  //   })
-  //   .then(resp => resp.json())
-  //   .then(data => console.log(data))
-  // }
+  componentDidMount() {
+    this.props.getProducts()
+    // fetch('http://localhost:3001/products', {
+    //   method: 'GET'
+    // })
+    // .then(resp => resp.json())
+    // .then(data => console.log(data))
+  }
 
   render() {
   const products = this.props.products.map((product, i) => <li key={i}>{product.name}</li>)
@@ -33,4 +35,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, {getProducts})(App);
