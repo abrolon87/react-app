8ee19fff087482def1618d6b627356b2432e0b61
Amanda <main@Amandas-MacBook-Air.local>
2020-08-12 03:59:57 -0400

    updates case statements

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,6 +1,17 @@
 export default (state = {products: [], loading: false}, action) => {
   switch(action.type){
+    case "LOADING_PRODUCTS":
+      return {
+        ...state, 
+        loading: true
+      }
 
+    case "PRODUCTS_LOADED":
+      return {
+        ...state, 
+        products: action.payload,
+        loading: false
+      }
     default: 
       return state
   }
