f5b7018bd9cf533c57be8c731fbad77b88e669a6
Amanda <main@Amandas-MacBook-Air.local>
2020-08-14 03:19:03 -0400

    adds PurposeForm.js

---
+++ frontend/src/components/PurposeForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PurposeForm extends Component {
+  render() {
+    return (
+      <div>
+        PURPOSES FORM
+      </div>
+    )
+  }
+}
+
+export default PurposeForm
