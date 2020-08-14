12050f303f032f151830b7eab4a996acdb6266d4
Amanda <main@Amandas-MacBook-Air.local>
2020-08-14 13:46:32 -0400

    builds Purposes.js; functional

--- frontend/src/components/Purposes.js
+++ frontend/src/components/Purposes.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Product from './Product'
 
 
 const Purposes = (props) => {
   
-  
+    console.log(props.purposes)
     return (
       <div>
-        Purposes
+        {/* first time load is undefined, this fixes that */}
+        {props.purposes && props.purposes.map(purpose =>   
+          <li key={purpose.id}>{purpose.body}</li>
+        )}
       </div>
     )
   
