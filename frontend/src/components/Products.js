9a93751621d1bf9545600b70a3b07158b24674a6
Amanda <main@Amandas-MacBook-Air.local>
2020-08-14 12:54:03 -0400

    builds Products.js; functioning

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,27 +1,33 @@
 import React from 'react'
+import {Route, Link} from 'react-router-dom'
+import Product from './Product'
 
-class Products extends React.Component {
-  // render() {
-  //   return (
-  //     <div>
-  //       PRODUCTS
-  //     </div>
-  //   )
-  // }
-  render() {
-    const products = this.props.products.map((product, i) => <li key={i}>{product.name}
-    <button id={product.id} onClick={this.handleClick} >X</button>
-    </li>)  //add a tag later 
-    return (
-      <div>
-        <h1>Click on a Product to see its uses:</h1>
-        <ul>
-          {this.props.loading ? <h3>Loading...</h3> : products}
-        </ul>
-        
-      </div>
-    )
-  }
+const Products = (props) => {
+  
+  return (
+    <div>
+      {props.products.map(product => 
+      <li key={product.id}>
+        <Link to={`/products/${product.id}`}>{product.name}</Link>
+        {/* <button id={product.id} >X</button>  */}
+      </li>)}
+    </div>
+  )
 }
 
-export default Products;
+export default Products
+  //   
+  // const products = this.props.products.map((product, i) => <li key={i}>{product.name}
+  //   <button id={product.id} onClick={this.handleClick} >X</button>
+  //   </li>)  //add a tag later 
+  //   return (
+  //     <div>
+  //       <h1>Click on a Product to see its uses:</h1>
+  //       <ul>
+  //         {this.props.loading ? <h3>Loading...</h3> : products}
+  //       </ul>
+        
+  //     </div>
+  //   )
+    
+  
