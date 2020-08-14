62c3ffff5905477bbc0a8280b19e7a1dbbbaad22
Amanda <main@Amandas-MacBook-Air.local>
2020-08-14 13:47:41 -0400

    builds form; not functioning

--- frontend/src/components/PurposeForm.js
+++ frontend/src/components/PurposeForm.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class PurposeForm extends Component {
+
+  state = {
+    body: ''
+  }
+
   render() {
     return (
       <div>
-        PURPOSES FORM
+        <form>
+         <label> Add a purpose for this product here: </label>
+           <input type="text" name="body" value={this.state.body}/>
+           <input type="submit" value="Add Purpose" />  
+        </form>
       </div>
     )
   }
 }
 
-export default PurposeForm
+export default connect(null)(PurposeForm)
