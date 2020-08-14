f20d8466d61843dd5e984efb07568c35c01ec746
Amanda <main@Amandas-MacBook-Air.local>
2020-08-14 15:58:32 -0400

    builds out PurposeForm with handlers; functioning

--- frontend/src/components/PurposeForm.js
+++ frontend/src/components/PurposeForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import { addPurpose } from '../actions/purposes'
 
 class PurposeForm extends Component {
 
@@ -7,17 +8,31 @@ class PurposeForm extends Component {
     body: ''
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addPurpose(this.state, this.props.product.id)
+    this.setState({
+      body: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
          <label> Add a purpose for this product here: </label>
-           <input type="text" name="body" value={this.state.body}/>
-           <input type="submit" value="Add Purpose" />  
+          <input type="text" name="body" value={this.state.body} onChange={this.handleChange} />
+          <input type="submit" value="Add Purpose" />  
         </form>
       </div>
     )
   }
 }
 
-export default connect(null)(PurposeForm)
+export default connect(null, {addPurpose})(PurposeForm)
