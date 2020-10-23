29746c8bbc53465500dbe6e8f14db0b9eaa3fa0e
Amanda Rolon <abrolon87@gmail.com>
2020-10-22 21:00:57 -0400

    clean up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'  //set up store
-import {composeWithDevTools} from 'redux-devtools-extension'
-import rootReducer from './reducers/index'
-import thunk from 'redux-thunk'
-import {Provider} from 'react-redux'
-import {BrowserRouter as Router} from 'react-router-dom'
-import './index.css';
 import App from './App';
-import Nav from './components/Nav'
-import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-    <Nav />
-    <App />
-    </Router>
-  </Provider>, 
-  document.getElementById('root'));
+    <App />,
+  document.getElementById('root')
+);
 
 
-serviceWorker.unregister();
+
