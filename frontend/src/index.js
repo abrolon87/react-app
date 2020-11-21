b7f74da94fe04ec03c2092544f2dca849b4eb8ec
Amanda Rolon <abrolon87@gmail.com>
2020-11-21 18:40:32 -0500

    creates store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import rootReducer from './reducers/index'
+
 import App from './App';
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
