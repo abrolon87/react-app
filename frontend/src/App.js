193a61564bc725f2986432ab69d63a537798cbb4
Amanda Rolon <abrolon87@gmail.com>
2020-11-21 18:40:04 -0500

    connects App.js to Redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './index.css';
 import ProductsContainer from './containers/ProductsContainer'
+import Nav from './components/Nav'
 import './App.css';
+import { getProducts } from './actions/products';
+
+
+
 class App extends Component {
+  
+  componentDidMount() {
+    this.props.getProducts()
+  }
+  
 
   render() {
     return (
       <div className="App">
-        <ProductsContainer />
+        
+        <Router>
+          <Nav />
+          <ProductsContainer />
+        </Router>
+        
       </div>
     )
   }
 }
 
-export default App;
+export default connect(null, { getProducts })(App)
